ae05c67be1c288fd2671082aac8e6534c90ce092
Aman Jee <amanjee@Amans-MacBook-Air.local>
2024-01-29 01:01:01 +0530

    Initial commit

---
+++ src/main/java/com/demo/quizapp/dao/QuestionDao.java
@@ -0,0 +1,7 @@
+package com.demo.quizapp.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class QuestionDao {
+}
---
+++ src/main/java/com/demo/quizapp/service/QuestionService.java
@@ -0,0 +1,13 @@
+package com.demo.quizapp.service;
+
+import com.demo.quizapp.Question;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class QuestionService {
+    public List<Question> getAllQuestions() {
+
+    }
+}
