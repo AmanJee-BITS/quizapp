06f7215990c3a99050d303fea8c7d21263484bd1
Aman Jee <amanjee@Amans-MacBook-Air.local>
2024-01-29 01:34:57 +0530

    Add dao layer

--- src/main/java/com/demo/quizapp/dao/QuestionDao.java
+++ src/main/java/com/demo/quizapp/dao/QuestionDao.java
@@ -1,7 +1,9 @@
 package com.demo.quizapp.dao;
 
+import com.demo.quizapp.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class QuestionDao {
+public interface QuestionDao extends JpaRepository<Question, Integer> {
 }
