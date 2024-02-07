f748b83301b94fbcac6d90fc0223f2fc1794f2e3
AmanJee-BITS <amangs896@gmail.com>
2024-02-08 01:02:41 +0530

    API to get a quiz questions

--- src/main/java/com/demo/quizapp/controller/QuizController.java
+++ src/main/java/com/demo/quizapp/controller/QuizController.java
@@ -1,12 +1,12 @@
 package com.demo.quizapp.controller;
 
+import com.demo.quizapp.model.QuestionWrapper;
 import com.demo.quizapp.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("quiz")
@@ -17,5 +17,10 @@ public class QuizController {
     @PostMapping("/create")
     public ResponseEntity<String> createQuiz(@RequestParam String language, @RequestParam int numQ, @RequestParam String title) {
         return quizService.createQuiz(language, numQ, title);
+    }
+
+    @GetMapping("/get/{quizId}")
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer quizId) {
+        return quizService.getQuizQuestions(quizId);
     }
 }
--- src/main/java/com/demo/quizapp/service/QuizService.java
+++ src/main/java/com/demo/quizapp/service/QuizService.java
@@ -3,13 +3,16 @@ package com.demo.quizapp.service;
 import com.demo.quizapp.dao.QuestionDao;
 import com.demo.quizapp.dao.QuizDao;
 import com.demo.quizapp.model.Question;
+import com.demo.quizapp.model.QuestionWrapper;
 import com.demo.quizapp.model.Quiz;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuizService {
@@ -28,5 +31,18 @@ public class QuizService {
         quizDao.save(quiz);
 
         return new ResponseEntity<>("Success", HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer quizId) {
+        Optional<Quiz> quiz = quizDao.findById(quizId);
+        List<Question> questionsFromDB = quiz.get().getQuestions();
+        List<QuestionWrapper> questionsForUser = new ArrayList<>();
+
+        for (Question q : questionsFromDB) {
+            QuestionWrapper qw = new QuestionWrapper(q.getId(), q.getQuestion(), q.getOption1(), q.getOption2(), q.getOption3(), q.getOption4());
+            questionsForUser.add(qw);
+        }
+
+        return new ResponseEntity<>(questionsForUser, HttpStatus.OK);
     }
 }
